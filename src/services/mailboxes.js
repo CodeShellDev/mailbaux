c1ff9d99ed6f595baf2665a5920f31ea8193a723
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2026-08-10 17:50:23 +0200

    fix missing esm converison in mailboxes.js

--- src/services/mailboxes.js
+++ src/services/mailboxes.js
@@ -1,20 +1,14 @@
-async function EditMailbox(id, email, { name }) {
+export async function EditMailbox(id, email, { name }) {
 	await db.UpdateBy(
 		{ id: id, "mailboxes.email": email },
 		{ "mailboxes.$.name": name },
 	)
 }
 
-async function CreateMailbox(id, { email, name }) {
+export async function CreateMailbox(id, { email, name }) {
 	await db.AddToArray({ id: id }, { mailboxes: { email, name } })
 }
 
-async function DeleteMailbox(id, email) {
+export async function DeleteMailbox(id, email) {
 	await db.DeleteFromArrayBy({ id: id }, { mailboxes: { email: email } })
-}
-
-module.exports = {
-	EditMailbox: EditMailbox,
-	CreateMailbox: CreateMailbox,
-	DeleteMailbox: DeleteMailbox,
 }
